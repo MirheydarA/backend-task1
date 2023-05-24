1ca464a7259c20f6e37dedbd5c33da975d59ef8b
MirheydarA <a.mirheyder004@gmail.com>
2023-05-24 12:19:56 +0400

    task added

---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name = "Heyder";
+            ViewData["Surname"] = "Alizade";
+            ViewBag.Age = 19;
+            ViewBag.Country = "Azerbaijan";
+            ViewBag.PhoneNumber = "0503903819";
+            //return RedirectToAction("Test");
+            return View();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+
+            app.MapControllerRoute(
+                name: "Default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            );
+
+            app.Run();
+
+        }
+    }
+}
